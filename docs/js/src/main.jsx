e70f0e157c1f32f70a885d6df21e78377581d706
Daniel Del Rio <ddelrio1986@gmail.com>
2017-01-04 14:46:57 -0500

    Test React setup

--- docs/js/src/main.jsx
+++ docs/js/src/main.jsx
@@ -1,1 +1,13 @@
-alert('test');
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class MyComponent extends React.Component {
+  render() {
+    return (
+      <h1>React Hello World</h1>
+    )
+  }
+}
+
+ReactDOM.render(<MyComponent />, document.getElementById('testRoot'))
