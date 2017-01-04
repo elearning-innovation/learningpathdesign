a02d0c5d67add57087c023453df007f2ffcd4f23
Daniel Del Rio <ddelrio1986@gmail.com>
2017-01-04 15:08:16 -0500

    Bootstrap test

--- docs/js/src/main.jsx
+++ docs/js/src/main.jsx
@@ -1,13 +1,5 @@
 
+import $ from 'jquery'
+import './bootstrap'
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-class MyComponent extends React.Component {
-  render() {
-    return (
-      <h1>React Hello World</h1>
-    )
-  }
-}
-
-ReactDOM.render(<MyComponent />, document.getElementById('testRoot'))
